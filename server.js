c55efbcfe142449eb7c0103f3d02ad93911e0198
richFrvr <ann.berries.14@gmail.com>
2021-09-25 19:56:22 +0000

    minor changes

--- server.js
+++ server.js
@@ -40,7 +40,8 @@ app.post('/', (req, res) => {
 
 })
 
-app.listen(process.env.PORT || 5000);
+app.listen(5000);
+console.log("Server started on port 5000"); 
 
 // static html code from document html to tbody
 const topCode = `<!DOCTYPE html>
